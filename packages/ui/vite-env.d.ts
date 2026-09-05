9a51772be943912dba3e4006719955d9a22260ce
taukhid <40711562+omaewa76@users.noreply.github.com>
2026-09-05 17:33:54 +0700

    update vite env

---
+++ packages/ui/vite-env.d.ts
@@ -0,0 +1,10 @@
+// packages/ui/src/vite-env.d.ts
+declare module '*.module.css' {
+  const classes: { readonly [key: string]: string };
+  export default classes;
+}
+
+declare module '*.module.scss' {
+  const classes: { readonly [key: string]: string };
+  export default classes;
+}
